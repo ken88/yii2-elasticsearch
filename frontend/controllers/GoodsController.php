5b2a40f84f392f626fe078dd8b4cb07ef327899d
WIN-BM6GDQQSQ2P\Administrator <yukai08621@163.com>
2021-03-17 15:51:10 +0800

    搜索完善

--- frontend/controllers/GoodsController.php
+++ frontend/controllers/GoodsController.php
@@ -5,6 +5,7 @@ namespace frontend\controllers;
 
 
 use common\models\Elastic;
+use function Webmozart\Assert\Tests\StaticAnalysis\object;
 use Yii;
 use yii\data\Pagination;
 
@@ -19,56 +20,100 @@ class GoodsController extends BaseController
         $res = $es::find();
         $hightlight = []; # 高亮
 
+        # 2. 接收参数
+        $cnName = Yii::$app->request->get('cn_name',''); # 商品名子
+        $price1 = Yii::$app->request->get('price1',0);   # 价钱大于
+        $price2 = Yii::$app->request->get('price2',0);   # 价钱小于
+        $priceOrder = Yii::$app->request->get('priceOrder',null);   # 价钱排序
+
+        $orderby = null;
+
+        # 2.1 排序
+        if (empty($priceOrder)) { # 没有排序条件默认 id倒序
+            $orderby = 'goods_id desc';
+        } else {
+            $orderby = $priceOrder == '价钱正序' ? 'shop_price asc' : 'shop_price desc';
+        }
 
         if (Yii::$app->request->get()) {
+            $query = []; # 查询条件
+            $i = 0;      # 记录搜素条件的下标
 
-            # 2. 接收参数
-            $cnName = Yii::$app->request->get('cn_name','');
-            $price1 = Yii::$app->request->get('price1',0);
-            $price2 = Yii::$app->request->get('price2',0);
-
+            # 3. 条件生成，商品名条件
             if (!empty($cnName)) {
-                # 2.1 高亮查询
-                $hightlight = [
-                    # 左标签，配合前端.highlight这个类来实现高亮
-                    "pre_tags"=>['<span style="color:red">'],
-                    "post_tags"=>['</span>'],
-
-                    # 在原生api中写的是{}表示空对象，因此使用php的stdClass来表示空对象
-                    "fields"=>[
-                        "cn_name"=>new \stdClass(),
-                        'descr'=>new \stdClass()
-                    ]
-                ];
-
-                # 2.2 通过DSL 查询es数据
-                $res = $res->query([
-                    'match' => [
-                        'cn_name' => [
+                $query['bool']['must'][$i] = [
+                    "match" => [
+                        "cn_name" => [
                             "query" => "{$cnName}",
                             "operator" => 'and'
                         ]
                     ]
-                ]);
+                ];
+                $i++;
             }
+
+            # 3.1 价钱大于某值 条件
+            if (!empty($price1)) {
+                $query['bool']['must'][$i] = [
+                    "range" => [
+                        "shop_price" => [
+                            "gte" => $price1
+                        ]
+                    ]
+                ];
+                $i++;
+            }
+
+            # 3.2 价钱小于某值 条件
+            if (!empty($price2)) {
+                $query['bool']['must'][$i] = [
+                    "range" => [
+                        "shop_price" => [
+                            "lte" => $price2
+                        ]
+                    ]
+                ];
+                $i++;
+            }
+//            dd($query);
+
+            # 3.3  高亮查询
+            $hightlight = [
+                # 左标签，配合前端.highlight这个类来实现高亮
+                "pre_tags"=>['<span style="color:red">'],
+                "post_tags"=>['</span>'],
+
+                # 在原生api中写的是{}表示空对象，因此使用php的stdClass来表示空对象
+                "fields"=>[
+                    "cn_name" => new \stdClass()
+                ]
+            ];
+
+            # 2.2 通过DSL 查询es数据
+            $res = $res->query($query);
         }
-        # 2.3 获取总数
+
+        # 4. 获取总数
         $count = $res->search(); # search 与 all 区别在于 all是在search基础上处理再拿出结果
 
+        # 4.1 获取分页
         $pages = new Pagination(['totalCount' => $count['hits']['total']['value']]);
 
-        $resInfo = $res->source(['cn_name','shop_price','original_img']) # 只显示的字段信息
-        ->highlight($hightlight) # 高亮数据
-        ->orderBy('goods_id asc') # 排序
-        ->offset($pages->offset)
-            ->limit(60)# 显示条数
+        # 4.2 数据获取
+        $resInfo = $res->source(['cn_name','shop_price','original_img']) # source 只显示的字段信息
+            ->highlight($hightlight) # 高亮数据
+            ->orderBy($orderby) # 排序
+            ->offset($pages->offset)
+            ->limit($pages->limit)# 显示条数
             ->asArray()
             ->all();
-
 
         $data = [
             'info' => $resInfo,
             'cn_name' => $cnName,
+            'price1' => $price1,
+            'price2' => $price2,
+            'priceOrder' => $priceOrder,
             'pages' => $pages
         ];
 
--- frontend/views/goods/index.php
+++ frontend/views/goods/index.php
@@ -12,6 +12,8 @@
         .cn_name{ height: 120px;}
         .price{ color: red}
         .m-pagination i{float: left;}
+        .color{color: red;}
+        .pagination li{ float: left; margin-left: 5px;list-style:none;}
     </style>
 </head>
 <body>
@@ -23,23 +25,33 @@
             <span><input type="text" id="cn_name" name="cn_name" value="<?php echo $cn_name;?>" /></span>
         </div>
         <div>
-            <span>价钱：</span>
-            <span><input type="text" id="price1" name="price1"> </span>
-            <span><input type="text" id="price2" name="price2"> </span>
+            <span>价钱：大于<input type="text" id="price1" name="price1" value="<?php echo $price1;?>"> </span>
+            <span>价钱：小于<input type="text" id="price2" name="price2" value="<?php echo $price2;?>"> </span>
             <span><input type="submit" value="搜索"> </span>
         </div>
         <div>
             <span>排序：</span>
             <span>
-                <a href="#">价钱正序</a>
+               <input type="submit" value="价钱正序" id="priceOrder1" name="priceOrder">
                 |
-                <a href="#" >价钱降序</a>
+                <input type="submit" value="价钱倒序" id="priceOrder2" name="priceOrder">
             </span>
         </div>
     </form>
 </div>
 
-
+<?php
+if (!empty($pages)) {
+    echo LinkPager::widget([
+        'pagination' => $pages,
+        'nextPageLabel' => '下一页',
+        'prevPageLabel' => '上一页',
+        'firstPageLabel' => '首页',
+        'lastPageLabel' => '尾页',
+        'maxButtonCount' => 20,
+    ]);
+}
+?>
 <div class="goods">
     <?php if (!empty($info)){foreach ($info as $v){ ?>
     <div class="info">
@@ -50,7 +62,6 @@
                 }else {
                     echo $v['_source']['cn_name'];
                 }
-
             ?>
         </div>
         <div>
@@ -60,18 +71,14 @@
     </div>
     <?php }} ?>
 </div>
-<?php
-if (!empty($pages)) {
-    echo LinkPager::widget([
-        'pagination' => $pages,
-        'nextPageLabel' => '下一页',
-        'prevPageLabel' => '上一页',
-        'firstPageLabel' => '首页',
-        'lastPageLabel' => '尾页',
-        'maxButtonCount' => 20,
 
-    ]);
-}
-?>
 </body>
 </html>
+<script type="text/javascript">
+    var priceOrder = '<?= $priceOrder;?>';
+    if (priceOrder == '价钱正序') {
+        document.getElementById('priceOrder1').className = 'color';
+    } else if (priceOrder == '价钱倒序')  {
+        document.getElementById('priceOrder2').className = 'color';
+    }
+</script>
